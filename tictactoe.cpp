f0c4e722935e9c5e3e6dbcb75579385eccc1cbec
Faisal <faisalalik9@gmail.com>
2021-02-19 23:59:44 +0530

    Final tictactoe

--- tictactoe.cpp
+++ tictactoe.cpp
@@ -6,7 +6,7 @@ class TictacToe{
 
     public:
 
-        char block[3][3]={{'1','1','1'},{'1','1','1'},{'1','1','1'}};
+        char block[3][3]={{' ',' ',' '},{' ',' ',' '},{' ',' ',' '}};
 
       
 
@@ -15,14 +15,25 @@ class TictacToe{
 
 
 
+        void show(){
+            for(int i=0;i<3;i++){
+                for(int j=0;j<3;j++){
+                    cout<<" | "<<block[i][j]<<" ";
+                }
+                cout<<" | ";
+                cout<<endl;
+                cout<<"___________________"<<endl;
+            }
+        }
 
 
 
+        void getMove(int pos, string p){
 
-        void getMove(int pos){
-            switch(pos){
+            if(p == "Player1"){
+                switch(pos){
                 case 1:
-                    if(block[0][0] == '1'){
+                    if(block[0][0] == ' '){
                             block[0][0] = 'X';
                             break;
                     }
@@ -30,11 +41,11 @@ class TictacToe{
                         cout<<"Invalid move try again"<<endl;
                         int pos;
                         cin>>pos;
-                        getMove(pos);
+                        getMove(pos,"Player1");
                     }
 
                 case 2:
-                    if(block[0][1] == '1'){
+                    if(block[0][1] == ' '){
                             block[0][1] = 'X';
                             break;
                     }
@@ -42,10 +53,10 @@ class TictacToe{
                         cout<<"Invalid move try again"<<endl;
                         int pos;
                         cin>>pos;
-                        getMove(pos);
+                        getMove(pos,"Player1");
                     }
                  case 3:
-                    if(block[0][2] == '1'){
+                    if(block[0][2] == ' '){
                             block[0][2] = 'X';
                             break;
                     }
@@ -53,10 +64,10 @@ class TictacToe{
                         cout<<"Invalid move try again"<<endl;
                         int pos;
                         cin>>pos;
-                        getMove(pos);
+                        getMove(pos,"Player1");
                     }
                  case 4:
-                    if(block[1][0] == '1'){
+                    if(block[1][0] == ' '){
                             block[1][0] = 'X';
                             break;
                     }
@@ -64,10 +75,10 @@ class TictacToe{
                         cout<<"Invalid move try again"<<endl;
                         int pos;
                         cin>>pos;
-                        getMove(pos);
+                        getMove(pos,"Player1");
                     }
                  case 5:
-                    if(block[1][1] == '1'){
+                    if(block[1][1] == ' '){
                             block[1][1] = 'X';
                             break;
                     }
@@ -75,10 +86,10 @@ class TictacToe{
                         cout<<"Invalid move try again"<<endl;
                         int pos;
                         cin>>pos;
-                        getMove(pos);
+                        getMove(pos,"Player1");
                     }
                  case 6:
-                    if(block[1][2] == '1'){
+                    if(block[1][2] == ' '){
                             block[1][2] = 'X';
                             break;
                     }
@@ -86,10 +97,10 @@ class TictacToe{
                         cout<<"Invalid move try again"<<endl;
                         int pos;
                         cin>>pos;
-                        getMove(pos);
+                        getMove(pos,"Player1");
                     }
                  case 7:
-                    if(block[2][0] == '1'){
+                    if(block[2][0] == ' '){
                             block[2][0] = 'X';
                             break;
                     }
@@ -97,10 +108,10 @@ class TictacToe{
                         cout<<"Invalid move try again"<<endl;
                         int pos;
                         cin>>pos;
-                        getMove(pos);
+                        getMove(pos,"Player1");
                     }
                  case 8:
-                    if(block[2][1] == '1'){
+                    if(block[2][1] == ' '){
                             block[2][1] = 'X';
                             break;
                     }
@@ -108,10 +119,10 @@ class TictacToe{
                         cout<<"Invalid move try again"<<endl;
                         int pos;
                         cin>>pos;
-                        getMove(pos);
+                        getMove(pos,"Player1");
                     }
                  case 9:
-                    if(block[2][2] == '1'){
+                    if(block[2][2] == ' '){
                             block[2][2] = 'X';
                             break;
                     }
@@ -119,14 +130,125 @@ class TictacToe{
                         cout<<"Invalid move try again"<<endl;
                         int pos;
                         cin>>pos;
-                        getMove(pos);
+                        getMove(pos,"Player1");
                     }
                 default:
                     cout<<"Invalid move try again"<<endl;
                         int pos;
                         cin>>pos;
-                        getMove(pos);
+                        getMove(pos,"Player1");
             }
+            }
+            else if(p == "Player2"){
+                switch(pos){
+                case 1:
+                    if(block[0][0] == ' '){
+                            block[0][0] = 'O';
+                            break;
+                    }
+                    else{
+                        cout<<"Invalid move try again"<<endl;
+                        int pos;
+                        cin>>pos;
+                        getMove(pos,"Player2");
+                    }
+
+                case 2:
+                    if(block[0][1] == ' '){
+                            block[0][1] = 'O';
+                            break;
+                    }
+                    else{
+                        cout<<"Invalid move try again"<<endl;
+                        int pos;
+                        cin>>pos;
+                        getMove(pos,"Player2");
+                    }
+                 case 3:
+                    if(block[0][2] == ' '){
+                            block[0][2] = 'O';
+                            break;
+                    }
+                    else{
+                        cout<<"Invalid move try again"<<endl;
+                        int pos;
+                        cin>>pos;
+                        getMove(pos,"Player2");
+                    }
+                 case 4:
+                    if(block[1][0] == ' '){
+                            block[1][0] = 'O';
+                            break;
+                    }
+                    else{
+                        cout<<"Invalid move try again"<<endl;
+                        int pos;
+                        cin>>pos;
+                        getMove(pos,"Player2");
+                    }
+                 case 5:
+                    if(block[1][1] == ' '){
+                            block[1][1] = 'O';
+                            break;
+                    }
+                    else{
+                        cout<<"Invalid move try again"<<endl;
+                        int pos;
+                        cin>>pos;
+                        getMove(pos,"Player2");
+                    }
+                 case 6:
+                    if(block[1][2] == ' '){
+                            block[1][2] = 'O';
+                            break;
+                    }
+                    else{
+                        cout<<"Invalid move try again"<<endl;
+                        int pos;
+                        cin>>pos;
+                        getMove(pos,"Player2");
+                    }
+                 case 7:
+                    if(block[2][0] == ' '){
+                            block[2][0] = 'O';
+                            break;
+                    }
+                    else{
+                        cout<<"Invalid move try again"<<endl;
+                        int pos;
+                        cin>>pos;
+                        getMove(pos,"Player2");
+                    }
+                 case 8:
+                    if(block[2][1] == ' '){
+                            block[2][1] = 'O';
+                            break;
+                    }
+                    else{
+                        cout<<"Invalid move try again"<<endl;
+                        int pos;
+                        cin>>pos;
+                        getMove(pos,"Player2");
+                    }
+                 case 9:
+                    if(block[2][2] == ' '){
+                            block[2][2] = 'O';
+                            break;
+                    }
+                    else{
+                        cout<<"Invalid move try again"<<endl;
+                        int pos;
+                        cin>>pos;
+                        getMove(pos,"Player2");
+                    }
+                default:
+                    cout<<"Invalid move try again"<<endl;
+                        int pos;
+                        cin>>pos;
+                        getMove(pos,"Player2");
+            }
+            }
+            
         }
 
         bool check(){
@@ -177,29 +299,46 @@ class TictacToe{
     
     TictacToe t;
 
-    int moves = 9;
+    int moves = 0;
     int pos;
+
+    string player1 ="Player1";
+    string player2 = "Player2";
 
     cout<<"Player 1 is X"<<endl;
     cout<<"Player 2 is O"<<endl;
 
-    while(moves-->0){
+    while(moves<9){
          cout<<t.p1<<" Enter your move : ";
          cin>>pos;
-         t.getMove(pos);
+         t.getMove(pos, player1);
+         t.show();
+         moves++;
          if(t.check()){
              moves = 0;
              break;
          }
         
+        
 
+        if(moves != 9){
          cout<<t.p2<<" Enter your move : ";
          cin>>pos;
-         t.getMove(pos);
+         t.getMove(pos, player2);
+         t.show();
+         moves++;
          if(t.check()){
              moves = 0;
              break;
          }
+
+         
+        }
+
+         
+
+
+         
 
     }
 
